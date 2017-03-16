b591dcfec5186cf8e26ffaf5a0d8c124f948f030
Paul Arelt <Paul Arelt>
2017-03-16 18:24:11 -0400

    first

---
+++ main.py
@@ -0,0 +1,91 @@
+# AUTHOR: PAUL ARELT
+
+import random
+
+
+def main():
+    values = ['']
+
+    hiringManager = str(input('Enter the Manager name\n'))
+    role = str(input('Enter the Role\n'))
+    company = str(input('Enter the Company name\n'))
+    requirements = str(input('Enter the job requirements as they appear'))
+
+    x = random.randrange(0, 3)
+    foo = open('out.txt', 'w')
+    templateNumber = 0
+    languages = "C, C++, Java, Javascript, Python, HTML and CSS"
+
+    if x == 1:
+        outt = ""
+        lines = []
+        nlines = []
+        templateNumber = 1
+        fileobj = open('templateOne.txt', 'r+')
+        values = "works hard, pays close attention to detail and works well with others"
+        proff = "I have experience working with C, C++, Java, Python, HTML, Javascript, CSS and Swift."
+        for xx in fileobj:
+            lines.append(xx.split(' '))
+        #print(lines)
+
+        for l in lines:
+            for ll in l:
+                if ll == '[manager]:\n':
+                    ll = ll.replace('[manager]:\n', hiringManager+':\n')
+                if ll == '[role]':
+                    ll = ll.replace('[role]', role)
+                if ll == '[values]':
+                    ll = ll.replace('[values]', values)
+                if ll == '[career_profile]':
+                    ll = ll.replace('[career_profile]', proff)
+                if ll == '[requirements]':
+                    ll = ll.replace('[requirements]', requirements)
+                nlines.append([ll])
+
+        for l in nlines:
+            for ll in l:
+                #print(ll)
+                outt += ll
+                outt += ' '
+
+        foo.write(outt)
+
+        foo.close()
+
+    else:
+        outt = ""
+        lines = []
+        nlines = []
+        templateNumber = 2
+        fileobj = open('templateTwo.txt', 'r+')
+        sellr = "I am a hard worker with a passion for coding. " \
+                "I believe that I can be a strong asset to "+company+" because of my strong knowledge in the languages described."
+        for xx in fileobj:
+            lines.append(xx.split(' '))
+
+        for l in lines:
+            for ll in l:
+                if ll == '[company]':
+                    ll = ll.replace('[company]', company)
+                if ll == '[manager]:\n':
+                    ll = ll.replace('[manager]:\n', hiringManager+':\n')
+                if ll == '[role]':
+                    ll = ll.replace('[role]', role)
+                if ll == '[languages]':
+                    ll = ll.replace('[languages]', languages)
+                if ll == '[seller]':
+                    ll = ll.replace('[seller]', sellr)
+                nlines.append([ll])
+
+        for l in nlines:
+            for ll in l:
+                #print(ll)
+                outt += ll
+                outt += ' '
+
+        foo.write(outt)
+
+        foo.close()
+
+
+main()
